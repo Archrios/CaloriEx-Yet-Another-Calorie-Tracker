2970d9a703d43ab26df693cdbba29a816eb8c67c
Jeffrey <jchen727@students.cs.ubc.ca>
2020-10-30 14:19:15 -0700

    Made applicaton robust; added method signatures

--- src/main/ui/CaloriEx.java
+++ src/main/ui/CaloriEx.java
@@ -29,14 +29,22 @@ public class CaloriEx {
     // main process is then run in a loop
     private void runCaloriEx() {
         boolean running;
-        boolean dayAdded;
-        String user;
+        boolean dayAdded = false;
+        String userInput;
+        int user = 0;
         input = new Scanner(System.in);
-        //dateMenu();
+
         do {
             startMenu();
-            dayAdded = startOperations();
+            try {
+                userInput = input.next();
+                user = Integer.parseInt(userInput);
+                dayAdded = startOperations(user);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid user input, please try again");
+            }
         } while (!dayAdded);
+
         do {
             running = dayOperations();
         } while (running);
@@ -54,31 +62,44 @@ public class CaloriEx {
             int year;
             int month;
             int date;
-            System.out.println("Enter the date you would like to add in the following format:");
-            System.out.println("YYYY MM DD");
-            year = Integer.parseInt(input.next());
-            month = Integer.parseInt(input.next());
-            date = Integer.parseInt(input.next());
-            //record.add(new Day(year, month, day));
-            day = new Day(year, month, date);
+            System.out.println("Enter the date you would like to add in the following format:\nYYYY-MM-DD");
+            try {
+                String inputs = input.next();
+                String[] dayFields = inputs.split("-");
+                year = Integer.parseInt(dayFields[0]);
+                month = Integer.parseInt(dayFields[1]);
+                date = Integer.parseInt(dayFields[2]);
+                day = new Day(year, month, date);
+            } catch (Exception e) {
+                return printAndReturn(e.toString() + " please try again", false);
+            }
         } else {
-            System.out.println("User input not recognized, please try again");
-            return false;
+            return printAndReturn("User input not recognized, please try again",false);
         }
-        jsonLocation += (day.getDate().toString() + ".json");
-        jsonWriter = new JsonWriter(jsonLocation);
-        jsonReader = new JsonReader(jsonLocation);
+        setJsonLocation(day.getDate().toString());
         return true;
     }
 
+    //MODIFIES: this
+    //EFFECTS: sets jsonLocation and builds new Writer and Reader based on that
+    //Mainly to circumvent 25 line limit
+    private void setJsonLocation(String date) {
+        jsonLocation += (day.getDate().toString() + ".json");
+        jsonWriter = new JsonWriter(jsonLocation);
+        jsonReader = new JsonReader(jsonLocation);
+    }
+
+    //EFFECTS: Displays the start menu
     private void startMenu() {
         System.out.println("\nWould you like to add a new day or load a previous day?");
         System.out.println("1 - New Day");
         System.out.println("2 - Load a previous date");
     }
 
-    private boolean startOperations() {
-        int user = input.nextInt();
+
+    //MODIFIES: this
+    //EFFECTS: adds new day or loads one based on user inputs
+    private boolean startOperations(int user) {
         switch (user) {
             case 1:
                 dateMenu();
@@ -92,8 +113,9 @@ public class CaloriEx {
         }
     }
 
+    //MODIFIES: this
+    //EFFECTS: attempts to load a day from a file
     private boolean loadDay() {
-
         boolean dateAdded = false;
         do {
             loadMenu();
@@ -110,6 +132,8 @@ public class CaloriEx {
         }
     }
 
+    //MODIFIES: this
+    //EFFECTS: loads a day based on user inputs
     private boolean loadOperations(String user) {
         String date;
         if (user.equals("t")) {
@@ -130,7 +154,7 @@ public class CaloriEx {
         return true;
     }
 
-    //EFFECTS: displays a menu of options for type of day to add
+    //EFFECTS: displays a menu of options for type of day to load
     private void loadMenu() {
         System.out.println("\nWhich day would you like to load from?");
         System.out.println("t - Today");
@@ -148,40 +172,53 @@ public class CaloriEx {
     //EFFECTS: displays main menu, and calls appropriate functions according to user input
     private boolean dayOperations() {
         mainMenu();
-        int user = Integer.parseInt(input.next());
-        switch (user) {
-            case 1:
-                addOperations();
-                break;
-            case 2:
-                caloricSummary();
-                break;
-            case 3:
-                recordOperations();
-                break;
-            case 4:
-                saveDay();
-                break;
-            case 0:
-                return false;
+        try {
+            int user = Integer.parseInt(input.next());
+            switch (user) {
+                case 1:
+                    addOperations();
+                    break;
+                case 2:
+                    caloricSummary();
+                    break;
+                case 3:
+                    recordOperations();
+                    break;
+                case 4:
+                    saveDay();
+                    break;
+                case 0:
+                    return false;
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            return printAndReturn(e.toString() + "please try again", true);
         }
-        return true;
     }
 
+    //EFFECTS: Prints out a given message and returns given boolean
+    // Mainly to circumvent the 25 line limit
+    private boolean printAndReturn(String message, boolean bool) {
+        System.out.println(message);
+        return bool;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: writes save file to desginated location
     private void saveDay() {
         try {
             jsonWriter.open();
             jsonWriter.write(day);
             jsonWriter.close();
-            System.out.println("saveeeeddddd");
+            System.out.println("File saved to: " + jsonLocation);
         } catch (FileNotFoundException e) {
-            System.out.println("nooot saved :(");
+            System.out.println("File not saved to: " + jsonLocation);
         }
     }
 
     //MODIFIES: this
     //EFFECTS: adds exercise to day using on user inputs
-    private void addExercise() {
+    private boolean addExercise() {
         input = new Scanner(System.in).useDelimiter("\n");
         String name;
         String desc;
@@ -191,7 +228,11 @@ public class CaloriEx {
         System.out.println("Please enter the name of the exercise.");
         name = input.next();
         System.out.println("Please enter the calories burned by the exercise.");
-        cals = Integer.parseInt(input.next());
+        try {
+            cals = Integer.parseInt(input.next());
+        } catch (NumberFormatException e) {
+            return printAndReturn(e.toString() + "please try again", false);
+        }
         if (hasDesc) {
             System.out.println("Please enter the description of the exercise.");
             desc = input.next();
@@ -199,13 +240,13 @@ public class CaloriEx {
         } else {
             day.addExercise(name, cals);
         }
-        System.out.println("The exercise: \"" + name + "\" has been added successfully");
         input = new Scanner(System.in);
+        return printAndReturn("The exercise: \"" + name + "\" has been added successfully", true);
     }
 
     //MODIFIES: this
     //EFFECTS: adds meal to day using user input
-    private void addMeal() {
+    private boolean addMeal() {
         input = new Scanner(System.in).useDelimiter("\n");
         String name;
         String desc;
@@ -215,7 +256,11 @@ public class CaloriEx {
         System.out.println("Please enter the name of the meal.");
         name = input.next();
         System.out.println("Please enter the calories in the meal.");
-        cals = Integer.parseInt(input.next());
+        try {
+            cals = Integer.parseInt(input.next());
+        } catch (NumberFormatException e) {
+            return printAndReturn(e.toString() + "please try again", false);
+        }
         if (hasDesc) {
             System.out.println("Please enter the description of the meal.");
             desc = input.next();
@@ -223,8 +268,9 @@ public class CaloriEx {
         } else {
             day.addMeal(name, cals);
         }
-        System.out.println("The meal: \"" + name + "\" has been added successfully");
         input = new Scanner(System.in);
+        return printAndReturn("The meal: \"" + name + "\" has been added successfully", true);
+
     }
 
     //EFFECTS: displays a menu of options to user
@@ -255,14 +301,23 @@ public class CaloriEx {
     //EFFECTS: calls methods to add meal or exercise based on user inputs
     private void addOperations() {
         additionMenu();
-        int user = Integer.parseInt(input.next());
-        switch (user) {
-            case 1:
-                addMeal();
-                break;
-            case 2:
-                addExercise();
-                break;
+        boolean added = false;
+        try {
+            int user = Integer.parseInt(input.next());
+            switch (user) {
+                case 1:
+                    do {
+                        added = addMeal();
+                    } while (!added);
+                    break;
+                case 2:
+                    do {
+                        added = addExercise();
+                    } while (!added);
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid user input, please try again");
         }
     }
 
@@ -278,23 +333,30 @@ public class CaloriEx {
     //EFFECTS: displays meal/exercise information based on user inputs
     private void recordOperations() {
         recordMenu();
-        int user = Integer.parseInt(input.next());
-        switch (user) {
-            case 1:
-                System.out.println("\n" + day.mealNames());
-                break;
-            case 2:
-                System.out.println("\n" + day.mealAllDetails());
-                break;
-            case 3:
-                System.out.println("\n" + day.exerciseNames());
-                break;
-            case 4:
-                System.out.println("\n" + day.exerciseAllDetails());
-                break;
+        try {
+            int user = Integer.parseInt(input.next());
+            switch (user) {
+                case 1:
+                    System.out.println("\n" + day.mealNames());
+                    break;
+                case 2:
+                    System.out.println("\n" + day.mealAllDetails());
+                    break;
+                case 3:
+                    System.out.println("\n" + day.exerciseNames());
+                    break;
+                case 4:
+                    System.out.println("\n" + day.exerciseAllDetails());
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            //return errorCaught(e.toString() + "please try again", true);
+            System.out.println("Invalid user input, please try again");
         }
     }
 
+    //MODIFIES: this
+    // resets the read/write location jsonLocation to default
     private void resetLocation() {
         jsonLocation = "./data/";
     }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -9,6 +9,7 @@ import persistence.Writable;
 import java.io.IOException;
 import java.time.LocalDate;
 
+//Main method, runs CaloriEx app
 public class Main {
 
     public static void main(String[] args) {
@@ -22,30 +23,30 @@ public class Main {
 //        System.out.println(input.next().equals("Y"));
 
 
-        String mdesc1 = "Big Mac, 10 piece Chicken Nuggets, with Fries and soda";
-        String mdesc2 = "California Roll, Dynamite Roll, with Salmon Sashimi";
-        String edesc1 = "100 Push ups, 100 Sit ups, 100 Squats and 10Km run";
-        String edesc2 = "1km free-style, 1km breast-stroke";
-        Day day = new Day();
-
-        day.addExercise("Swimming", edesc2, 650);
-        day.addExercise("Gym", edesc1, 450);
-        day.addExercise("Grouse Grind", 350);
-        day.addMeal("Sushi", mdesc2, 550);
-        day.addMeal("McDonalds", mdesc1, 725);
-        day.addMeal("Cereal with Milk", 275);
-
-        JSONObject json = day.toJson();
-
-        JsonWriter write = new JsonWriter("./data/2020-10-29.json");
-        try {
-            write.open();
-            write.write(day);
-            write.close();
-        } catch (IOException e) {
-            return;
-        }
-        //new CaloriEx();
+//        String mdesc1 = "Big Mac, 10 piece Chicken Nuggets, with Fries and soda";
+//        String mdesc2 = "California Roll, Dynamite Roll, with Salmon Sashimi";
+//        String edesc1 = "100 Push ups, 100 Sit ups, 100 Squats and 10Km run";
+//        String edesc2 = "1km free-style, 1km breast-stroke";
+//        Day day = new Day();
+//
+//        day.addExercise("Swimming", edesc2, 650);
+//        day.addExercise("Gym", edesc1, 450);
+//        day.addExercise("Grouse Grind", 350);
+//        day.addMeal("Sushi", mdesc2, 550);
+//        day.addMeal("McDonalds", mdesc1, 725);
+//        day.addMeal("Cereal with Milk", 275);
+//
+//        JSONObject json = day.toJson();
+//
+//        JsonWriter write = new JsonWriter("./data/2020-10-29.json");
+//        try {
+//            write.open();
+//            write.write(day);
+//            write.close();
+//        } catch (IOException e) {
+//            return;
+//        }
+        new CaloriEx();
 
     }
 }
