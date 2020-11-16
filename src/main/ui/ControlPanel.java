47653db3e1e4c127ed460f8582ae8e77d5464661
Jeffrey <jchen727@students.cs.ubc.ca>
2020-11-15 18:26:14 -0800

    added method specifications and comments

--- src/main/ui/ControlPanel.java
+++ src/main/ui/ControlPanel.java
@@ -9,37 +9,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 
+// main panel class that holds other panels
 public class ControlPanel extends JPanel {
     private String jsonLocation = "./data/";
     private final File errorSoundFile = new File("data/Sounds/Error.wav").getAbsoluteFile();
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private Day day;
-    private final StartPanel startPanel;
-    private final DatePanel datePanel;
-    private final LoadPanel loadPanel;
-    private final OperationPanel operationPanel;
-    private final AddPanel addPanel;
-    private final DetailsPanel detailsPanel;
+    private StartPanel startPanel;
+    private DatePanel datePanel;
+    private LoadPanel loadPanel;
+    private OperationPanel operationPanel;
+    private AddPanel addPanel;
+    private DetailsPanel detailsPanel;
     private static final int FIELD_WIDTH = 5;
 
 
+    //Start screen of the application
     private class StartPanel extends JPanel {
         private final JLabel label;
         private final JButton newButton;
         private final JButton loadButton;
 
+        //MODIFIES: this
+        //EFFECTS: Constructor, creates JComponents and adds them to this
         public StartPanel() {
             label = new JLabel("Would you like to add a new day or load a previous day?");
             newButton = newDayButton("New Day");
             loadButton = loadDayButton("Load a previous date");
-            //this.setLayout(new GridLayout(3,1));
             newButton.setAlignmentX(CENTER_ALIGNMENT);
             label.setAlignmentX(CENTER_ALIGNMENT);
             loadButton.setAlignmentX(CENTER_ALIGNMENT);
@@ -53,6 +57,8 @@ public class ControlPanel extends JPanel {
         }
 
 
+        // MODIFIES: this, datePanel
+        //EFFECTS: creates a button that sets this panel to invisible adn datePanel to be visible
         private JButton newDayButton(String title) {
             class NewListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -66,6 +72,8 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        // MODIFIES: this, loadPanel
+        //EFFECTS: creates a button that sets this panel to invisible adn loadPanel to be visible
         private JButton loadDayButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -80,11 +88,14 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    //Load screen of the application
     private class LoadPanel extends JPanel {
         private final JLabel label;
         private final JButton todayButton;
         private final JButton selectButton;
 
+        //MODIFIES: this
+        //EFFECTS: Constructor, creates JComponents and adds them to this
         public LoadPanel() {
             label = new JLabel("Which day would you like to load from?");
             todayButton = createTodayButton("Today");
@@ -101,6 +112,10 @@ public class ControlPanel extends JPanel {
             this.add(selectButton);
         }
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: creates a button that attempts to load Day from set JsonLocation. if successful, panel gets
+        // set to invisible, operationPanel get set to Visible. if unsuccessful, plays error sound and resets
+        //jsonLocation to default. A message pane is sent in either scenario
         private JButton createTodayButton(String title) {
             class NewListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -124,6 +139,11 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: creates a button that attempts to load Day from user input. if successful, panel gets
+        // set to invisible, operationPanel get set to Visible. if unsuccessful, plays error sound and resets
+        //jsonLocation to default. A message pane is sent in either scenario. this process is looped until a file
+        // is loaded
         private JButton createSelectButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -152,11 +172,14 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    //date panel where user selects which date to add to
     private class DatePanel extends JPanel {
         private final JLabel label;
         private final JButton todayButton;
         private final JButton selectButton;
 
+        //MODIFIES: this
+        //EFFECTS: Constructor, creates JComponents and adds them to this
         public DatePanel() {
             label = new JLabel("Which day would you like to add to?");
             todayButton = createTodayButton("Today");
@@ -173,6 +196,9 @@ public class ControlPanel extends JPanel {
             this.add(selectButton);
         }
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: creates a button that creates a new Day of today. jsonLocation is then set to that date
+        // this is then set to be invisible and operationPanel set to visible
         private JButton createTodayButton(String title) {
             class NewListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -188,7 +214,10 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
-        private boolean addDay() {
+        //MODIFIES: this
+        //EFFECTS: takes user input and attempts to set day accordingly. if unsuccessful, plays error sound and
+        // displays corresponding message to user. this process is looped until day is added successfully
+        private void addDay() {
             boolean dateAdded = false;
             String inputs;
             do {
@@ -212,16 +241,14 @@ public class ControlPanel extends JPanel {
                     break;
                 }
             } while (!dateAdded);
-            return dateAdded;
         }
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: adds day, sets jsonlocation accordingly, sets this to be invisible and operationPanel visible
         private JButton createSelectButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
-                    boolean dayAdded = false;
-                    do {
-                        dayAdded = addDay();
-                    } while (!dayAdded);
+                    addDay();
                     setJsonLocation();
                     operationPanel.setVisible(true);
                     setVisible(false);
@@ -234,6 +261,7 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    //ui screen/panel for adding activites to the day
     private class AddPanel extends JPanel {
         //        private JLabel label = new JLabel("What would you like to add");
 //        private JTextField name = new JTextField(FIELD_WIDTH);
@@ -294,6 +322,9 @@ public class ControlPanel extends JPanel {
 //            panel.add(Box.createRigidArea(new Dimension(0,10)));
 //        }
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: takes in user input and attempts to adds meal/exercise. if successful, sets visibility to false
+        // and visibility of operationPanel to true. otherwise plays error sound file and displays message
         private JButton createSubmitButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -321,12 +352,15 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        //MODIFES: this
+        //EFFECTS: creates custom JComponents for constructor from .form file
         private void createUIComponents() {
             panel = this;
             submitButton = createSubmitButton("Submit");
         }
     }
 
+    //UI screen/panel for users to submit what details they would like to see about meals/exercise
     class DetailsPanel extends JPanel {
 
         private ButtonGroup buttonGroup;
@@ -338,8 +372,11 @@ public class ControlPanel extends JPanel {
         private JLabel label;
         private JPanel panel;
 
+        //MODIFIES: this, operationPanel
+        //EFFECTS: takes user input and displays details of meals/exercises accordingly. if list of meal/activity is
+        //empty, displays that the list is empty. this panel is then set invisible and operationPanel visible
         private JButton createSubmitButton(String title) {
-            class LoadListener implements ActionListener {
+            class SubmitListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
                     String message = "";
                     if (mealListRadioButton.isSelected()) {
@@ -362,10 +399,12 @@ public class ControlPanel extends JPanel {
             }
 
             JButton button = new JButton(title);
-            button.addActionListener(new LoadListener());
+            button.addActionListener(new SubmitListener());
             return button;
         }
 
+        //MODIFES: this
+        //EFFECTS: creates custom JComponents for constructor from .form file
         private void createUIComponents() {
             panel = this;
             submitButton = createSubmitButton("Submit");
@@ -373,6 +412,7 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    // operation screen/panel where users choose what actions they would like to do
     private class OperationPanel extends JPanel {
         private final JLabel label;
         private final JButton addButton;
@@ -381,6 +421,8 @@ public class ControlPanel extends JPanel {
         private final JButton saveButton;
         private final JButton exitButton;
 
+        //MODIFIES: this
+        //EFFECTS: Constructor, creates JComponents and adds them to this
         public OperationPanel() {
             label = new JLabel("What would you like to do");
             addButton = createAddButton("Add a meal/exercise");
@@ -397,6 +439,8 @@ public class ControlPanel extends JPanel {
             setandAddComponent(this, exitButton);
         }
 
+        //MODIFIES: this, addPanel
+        //EFFECTS: creates a button that sets this to be invisible and addPanel Visible
         private JButton createAddButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -410,6 +454,7 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        //EFFECTS: creates a button that displays summary of calorie totals from meals and exercises
         private JButton createCalorieButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -427,6 +472,8 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        //MODIFIES: this, addPanel
+        //EFFECTS: creates a button that sets this to be invisible and detailsPanel Visible
         private JButton createDetailsButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -440,6 +487,9 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        //MODIFIES: this
+        //EFFECTS: creates a button that attempts to save the day to jsonLocation. plays error sound if unsuccessful
+        // displays message accordingly whether succesful or not
         private JButton createSaveButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
@@ -449,6 +499,7 @@ public class ControlPanel extends JPanel {
                         jsonWriter.close();
                         JOptionPane.showMessageDialog(null, "File saved to: " + jsonLocation);
                     } catch (FileNotFoundException e) {
+                        playClip(errorSoundFile);
                         JOptionPane.showMessageDialog(null, "File not saved to: " + jsonLocation);
                     }
                 }
@@ -459,10 +510,13 @@ public class ControlPanel extends JPanel {
             return button;
         }
 
+        // MODIFIES: this
+        //EFFECTS: displays a message then exits the JFRame using a helper method
         private JButton createExitButton(String title) {
             class LoadListener implements ActionListener {
                 public void actionPerformed(ActionEvent event) {
-
+                    JOptionPane.showMessageDialog(null,"Thanks for using CaloriEx, hope you enjoyed it!!");
+                    exit();
                 }
             }
 
@@ -473,6 +527,8 @@ public class ControlPanel extends JPanel {
     }
 
 
+    //MODIFIES: this
+    //EFFECTS: constructor for Control Panel; adds panels and sets visibility
     public ControlPanel() {
         startPanel = new StartPanel();
         datePanel = new DatePanel();
@@ -495,7 +551,15 @@ public class ControlPanel extends JPanel {
         detailsPanel.setVisible(false);
     }
 
+    //MODIFIES: this
+    //EFFECTS: closes window of parent frame
+    public void exit() {
+        JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class, this);
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    }
 
+
+    // Listener class that ensures clip is played in full
     // modeled after https://stackoverflow.com/questions/577724/trouble-playing-wav-in-java/577926#577926
     static class AudioListener implements LineListener {
         private boolean done = false;
@@ -516,6 +580,7 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    //EFFECTS: plays an audio clip until its completed playing using a helper class
     // modeled after https://stackoverflow.com/questions/577724/trouble-playing-wav-in-java/577926#577926
     private static void playClip(File clipFile) {
         AudioListener listener = new AudioListener();
@@ -539,6 +604,8 @@ public class ControlPanel extends JPanel {
         }
     }
 
+    //MODIFIES: JPanel
+    //EFFECTS: adds jcomponent to jpanel with Center Alignment, and adds a box for spacing
     public void setandAddComponent(JPanel panel, JComponent j) {
         j.setAlignmentX(CENTER_ALIGNMENT);
         panel.add(j);
@@ -563,12 +630,16 @@ public class ControlPanel extends JPanel {
         jsonLocation = "./data/";
     }
 
+    //MODIFIES: this
+    //EFFECTS:  sets the read/write location jsonLocation and creates new jsonWriter and jsonReader with that location
     public void setJsonLocation() {
         jsonLocation += (day.getDate().toString() + ".json");
         jsonWriter = new JsonWriter(jsonLocation);
         jsonReader = new JsonReader(jsonLocation);
     }
 
+    //MODIFIES: this
+    //EFFECTS: sets read/write location jsonLocation and creates new jsonWriter and jsonReader using the given String
     public void setJsonLocation(String date) {
         jsonLocation += (date + ".json");
         jsonWriter = new JsonWriter(jsonLocation);
